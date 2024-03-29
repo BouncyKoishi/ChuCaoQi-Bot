a154f73fcb35aea7f8b1db54a61d75f7881e1754
BouncyKoishi <bouncykoishi@qq.com>
2024-03-29 18:00:03 +0800

    gocqApi 新增发送合并消息api

--- gocq_direct.py
+++ gocq_direct.py
@@ -11,6 +11,32 @@ def setGroupPortrait(group_id: int, file: str):
     return _link("set_group_portrait", dataDict)
 
 
+def sendForwardMessage(messages: list, group_id=None, user_id=None):
+    if group_id:
+        return sendGroupForwardMessage(group_id, messages)
+    elif user_id:
+        return sendPrivateForwardMessage(user_id, messages)
+    else:
+        print("cqAPI 请求失败: 未指定发送目标")
+        return None
+
+
+def sendPrivateForwardMessage(user_id: int, messages: list):
+    dataDict = {
+        "user_id": user_id,
+        "messages": messages
+    }
+    return _link("send_private_forward_msg", dataDict)
+
+
+def sendGroupForwardMessage(group_id: int, messages: list):
+    dataDict = {
+        "group_id": group_id,
+        "messages": messages
+    }
+    return _link("send_group_forward_msg", dataDict)
+
+
 # 直接调用go-cqhttp的正向HTTP Api
 def _link(api: str, data: dict):
     goCqLink = f"http://localhost:{HTTP_PORT}/"
