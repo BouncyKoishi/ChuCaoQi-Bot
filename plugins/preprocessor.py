05250b6e0c40d94b1551c5c0780815f5b7d352b1
BouncyKoishi <bouncykoishi@qq.com>
2024-09-12 19:06:37 +0800

    优化好友码和刷指令判定

--- plugins/preprocessor.py
+++ plugins/preprocessor.py
@@ -23,18 +23,18 @@ async def func(bot: nonebot.NoneBot, event: aiocqhttp.Event, plugin_manager: non
         
         # 记录触发指令时间，屏蔽刷指令
         warningCount = repeatWarning.get(event.user_id)
-        if warningCount and warningCount >= 3:
+        if warningCount and warningCount >= 8:
             raise nonebot.message.CanceledException("刷指令人员，暂时屏蔽所有服务")
         if event.user_id in lastSpellRecord:
             recordTimeStamp = lastSpellRecord[event.user_id]
-            if time.time() - recordTimeStamp <= 0.25:
+            if time.time() - recordTimeStamp <= 0.5:
                 if warningCount:
                     warningCount += 1
                 else:
                     warningCount = 1
                 repeatWarning[event.user_id] = warningCount
-                if warningCount >= 3:
-                    msg = '识别到恶意刷指令。除草器所有服务对你停止3小时。'
+                if warningCount >= 8:
+                    msg = '识别到恶意刷指令。除草器所有服务对你停止1小时。'
                     if event.detail_type == "group":
                         await bot.send_group_msg(group_id=event.group_id, message=msg)
                     else:
@@ -46,6 +46,6 @@ async def func(bot: nonebot.NoneBot, event: aiocqhttp.Event, plugin_manager: non
 async def cleanWarning():
     global repeatWarning
     for key in repeatWarning:
-        if time.time() - lastSpellRecord[key] > 3 * 3600:
+        if time.time() - lastSpellRecord[key] > 3600:
             repeatWarning[key] = 0
 
