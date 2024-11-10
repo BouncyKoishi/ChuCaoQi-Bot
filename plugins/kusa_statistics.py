a70a28d8fe9eded44b5967593325b5d0dc6f85e7
BouncyKoishi <bouncykoishi@qq.com>
2024-11-10 18:49:17 +0800

    新增可配置的草精排行榜

--- plugins/kusa_statistics.py
+++ plugins/kusa_statistics.py
@@ -45,13 +45,12 @@ async def _(session: CommandSession):
     userList = await baseDB.getAllUser()
     userList = sorted(userList, key=lambda x: x.kusa, reverse=True)
     outputStr = "草排行榜：\n"
-    for i in range(25):
-        if i >= len(userList):
-            break
-        user = userList[i]
+    for i, user in enumerate(userList[:25]):
+        if user.qq == config['qq']['bot']:
+            continue
         userName = user.name if user.name else user.qq
         outputStr += f'{i + 1}. {userName}: {user.kusa}\n'
-    await session.send(outputStr)
+    await session.send(outputStr[:-1])
 
 
 @on_command(name='FACTORY_RANK', only_to_me=False)
@@ -61,14 +60,11 @@ async def _(session: CommandSession):
 
     factoryList = await itemDB.getStoragesOrderByAmountDesc("生草工厂")
     outputStr = "工厂数排行榜：\n"
-    for i in range(25):
-        if i >= len(factoryList):
-            break
-        info = factoryList[i]
+    for i, info in enumerate(factoryList[:25]):
         user = await baseDB.getUser(info.qq)
         userName = user.name if user.name else user.qq
         outputStr += f'{i + 1}. {userName}: {info.amount}\n'
-    await session.send(outputStr)
+    await session.send(outputStr[:-1])
 
 
 @on_command(name='KUSA_ADV', only_to_me=False)
@@ -88,7 +84,7 @@ async def _(session: CommandSession):
     await session.send(outputStr)
 
 
-@on_command(name='KUSA_ADV_RANK', only_to_me=False, aliases='草精排行榜')
+@on_command(name='草精排行榜', only_to_me=False)
 async def _(session: CommandSession):
     if not await permissionCheck(session):
         return
@@ -96,11 +92,29 @@ async def _(session: CommandSession):
     await session.send(outputStr)
 
 
-@on_command(name='KUSA_ADV_RANK2', only_to_me=False, aliases='草精新星榜')
+@on_command(name='草精新星榜', only_to_me=False)
 async def _(session: CommandSession):
     if not await permissionCheck(session):
         return
     outputStr = '草精新星排行榜：' + await getKusaAdvRank(levelMax=6)
+    await session.send(outputStr)
+
+
+@on_command(name='KUSA_ADV_RANK', only_to_me=False)
+async def _(session: CommandSession):
+    if not await isSuperAdmin(session.ctx['user_id']):
+        return
+    strippedArg = session.current_arg_text.strip()
+    showInactiveUsers = True if '-i' in strippedArg else False
+    showSubAccount = True if '-s' in strippedArg else False
+    levelMax = 10
+    if '--l' in strippedArg:
+        try:
+            levelMax = int(strippedArg.split('--l')[1].strip().split()[0])
+        except (IndexError, ValueError):
+            await session.send("Invalid levelMax value. Please provide a valid integer after --l.")
+            return
+    outputStr = '草精排行榜（自定义）：' + await getKusaAdvRank(levelMax, showInactiveUsers, showSubAccount)
     await session.send(outputStr)
 
 
@@ -117,19 +131,22 @@ async def permissionCheck(session: CommandSession) -> bool:
         return False
 
 
-async def getKusaAdvRank(levelMax: int = 10):
+async def getKusaAdvRank(levelMax: int = 10, showInactiveUsers: bool = False, showSubAccount: bool = True) -> str:
     userList = await baseDB.getAllUser()
     userAdvKusaDict = {}
     for user in userList:
         if user.vipLevel > levelMax:
             continue
+        if not showSubAccount and user.relatedQQ:
+            continue
         bluePrintExist = await itemDB.getItemAmount(user.qq, '生草工业园区蓝图')
         if not bluePrintExist:
             continue
-        # 近90天无生草记录，则不显示在排行榜
-        kusaRecordRow = await fieldDB.kusaHistoryReport(user.qq, datetime.now(), 7776000)
-        if kusaRecordRow["count"] == 0:
-            continue
+        if not showInactiveUsers:
+            # 近90天无生草记录，则不显示在排行榜
+            kusaRecordRow = await fieldDB.kusaHistoryReport(user.qq, datetime.now(), 7776000)
+            if kusaRecordRow["count"] == 0:
+                continue
         total, _, _, _ = await getKusaAdv(user)
         userAdvKusaDict[user] = total
     userAdvKusaDict = sorted(userAdvKusaDict.items(), key=lambda x: x[1], reverse=True)
@@ -139,7 +156,7 @@ async def getKusaAdvRank(levelMax: int = 10):
         user = userAdvKusaDict[i][0]
         userName = user.name if user.name else user.qq
         outputStr += f'{i + 1}. {userName}: {userAdvKusaDict[i][1]}\n'
-    return outputStr
+    return outputStr[:-1]
 
 
 async def getKusaAdv(user):
