27004027c1cbdb886d9168d6d4acddf91bf18ef0
BouncyKoishi <bouncykoishi@qq.com>
2025-12-03 20:02:51 +0800

    调整log

--- plugins/chatGPT_api.py
+++ plugins/chatGPT_api.py
@@ -458,4 +458,4 @@ async def permissionCheck(session: CommandSession, checker: str):
 @scheduler.scheduled_job('cron', hour=3, minute=1, misfire_grace_time=None)
 async def resetTodayTokenUseRunner():
     await db.resetTodayTokenUse()
-    await sendLog("已重置所有用户的todayTokenUse")
+    print("已重置所有用户的todayTokenUse")
